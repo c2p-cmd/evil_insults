c07712f51a39f017dd5954c8c7d0cd8b688a11ca
Sharan Thakur <sharandt19@live.com>
2024-01-03 11:08:13 +0530

    Changed to iOS

---
+++ EvilInsults/EvilInsultsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EvilInsultsApp.swift
+//  EvilInsults
+//
+//  Created by Sharan Thakur on 03/01/24.
+//
+
+import SwiftUI
+
+@main
+struct EvilInsultsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
